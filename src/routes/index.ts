49d16eefc4948d9061502a4602a97ca85bdf39c3
Aimeana100 <aimeanathole@gmail.com>
2023-06-05 19:56:10 +0200

    Fix deployment and discriminate docker-compose file THis Commit has the changes that distructure the docker-compose.yml script from backend and froneend

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,7 +10,7 @@ router.use('/auth', authRoutes);
 router.use('/seeds', seedRoutes);
 router.use('/order', orderRoutes);
 router.use('/fertilizer', fertilizerRoutes);
-router.use('*', (req: Request, res: Response) => {
+router.use('/*', (req: Request, res: Response) => {
   return res.status(200).json({ status: 'success', message: 'This is default routes' });
 });
 export default router;
